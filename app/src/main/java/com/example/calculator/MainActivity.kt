5371ef5df3321ce12479202bf31ef577a9e95138
RusdyZain <ryanfajri28@gmail.com>
2024-01-14 08:02:59 +0800

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.components.Calculator
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel : CalculatorViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CalculatorTheme {
+                val state = viewModel.state
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Calculator(
+                        state = state, modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        onAction = viewModel::onAction,
+                        buttonSpacing = 12.dp
+                    )
+                }
+            }
+        }
+    }
+}
